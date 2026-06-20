73b643cc5d1d657cb750302b5388c5709d1b4355
Victor Santos <menezes.victorsantos@outlook.com>
2026-06-20 05:12:00 -0300

    feat: added new skiils at skiils.tsx
    This commit also includes the following changes:
    - refactor: better responsive layout for skills page.
    - refactor: better responsive design for `IdeComponents` as well.

--- components/core/pages/skills/skills.tsx
+++ components/core/pages/skills/skills.tsx
@@ -7,26 +7,49 @@ const items: { name: string, value: number, comment?: string }[] = [
         value: 85
     },
     {
+        name: "Python",
+        value: 48,
+        comment: "Cursando CS50P de Harvard"
+    },
+    {
+        name: "Dev com IA",
+        value: 90,
+        comment: "3x Anthropic Claude code and AI Fluency"
+    },
+    {
+        name: "AWS Cloud",
+        value: 70,
+        comment: "Em proceso para Cloud Practitioner."
+    },
+    {
+        name: "Git & CI/CD",
+        value: 70,
+    },
+    {
+        name: "Inglês",
+        value: 90,
+        comment: "Nível C1"
+    },
+    {
         name: "Node.js",
         value: 80
     },
     {
-        name: "Prisma ORM",
+        name: "NestJs",
+        value: 70
+    },
+    {
+        name: "Docker",
+        value: 65
+    },
+    {
+        name: "React",
         value: 75
     },
     {
-        name: "Next.js",
-        value: 80
-    },
-    {
-        name: "Postgres",
+        name: "Postgres + ORMs",
         value: 75
     },
-    {
-        name: "AWS Cloud",
-        value: 80,
-        comment: "Em proceso para Cloud Practitioner."
-    }
 ]
 
 export default function Skills() {
@@ -37,13 +60,13 @@ export default function Skills() {
                     {items.map((item) => (
                         <div className="flex flex-col gap-1" key={item.name}>
                             <IdeTextGroup curlyBrackets>
-                                <div className="flex gap-2 items-center">
-                                    <IdeTextHighlighted variant={"dotted"}>"name"{":"} "{item.name}"</IdeTextHighlighted>
-                                    <IdeTextHighlighted variant={"dotted"}>"level" {":"} "{item.value}"</IdeTextHighlighted>
-                                    {item.comment ? <IdeComment>{item.comment}</IdeComment> : null}
+                                <div className="flex gap-2 items-center border max-sm:grid max-sm:grid-cols-[auto_1fr]">
+                                    <IdeTextHighlighted variant={"dotted"} className="whitespace-nowrap">"name"{":"} <b>"{item.name}"</b></IdeTextHighlighted>
+                                    <IdeTextHighlighted variant={"dotted"} className="whitespace-nowrap max-sm:justify-self-end">"level"{":"} <b>"{item.value}"</b></IdeTextHighlighted>
+                                    {item.comment ? <IdeComment className="max-sm:col-span-2">{item.comment}</IdeComment> : null}
                                 </div>
-                                <div className="flex w-full justify-between items-center">
-                                    <IdeText><b>{item.name}</b></IdeText> <Level className={"w-10/12"} value={item.value}>{item.value}</Level>
+                                <div className="flex w-full gap-2 justify-between items-center">
+                                    <IdeText className="whitespace-nowrap max-sm:hidden"><b>{item.name}</b></IdeText> <Level className={"w-10/12"} value={item.value}>{item.value}</Level>
                                 </div>
                             </IdeTextGroup>
                         </div>
